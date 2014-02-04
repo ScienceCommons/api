29d543fc5258e54da72cf24c61793a29d3b24019
Benjamin Coe <ben@yesware.com>
2014-02-03 21:38:18 -0800

    initial commit of rails site.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ScienceCommonsApi::Application.config.secret_key_base = 'd410eb964ae4915ce2b72ef8036c0a3b9b84974d55d68c29099f721504396f32e877d8c3dfb654bd5fdfc627026ca288afa84206b76cc9ebaf446191a6a335b4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ScienceCommonsApi::Application.config.session_store :cookie_store, key: '_science_commons_api_session'
