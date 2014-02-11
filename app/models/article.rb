ff8db20407a2eebadd28a036479d0d0f32ef2c8b
Benjamin Coe <ben@yesware.com>
2014-02-11 08:49:53 -0800

    added example unit tests for articles.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,4 @@
 class Article < ActiveRecord::Base
+  validates_uniqueness_of :doi
+  validates_presence_of :doi
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,8 +1,29 @@
 require 'spec_helper'
 
 describe Article do
-  it "should not allow two articles with the same DOI to be created" do
-    Article.create(doi: '123banana')
-    expect { Article.create(doi: '123banana') }.to raise_error(ActiveRecord::RecordNotUnique)
+
+  describe "doi" do
+    it "should not allow two articles with the same DOI to be created" do
+      Article.create(doi: '123banana')
+      article = Article.create(doi: '123banana')
+
+      article.errors.count.should == 1
+
+      field, error = article.errors.first
+
+      field.should == :doi
+      error.should == 'has already been taken'
+    end
+
+    it "should not allow an article with a nil DOI to be created" do
+      article = Article.create(doi: nil)
+
+      article.errors.count.should == 1
+
+      field, error = article.errors.first
+      field.should == :doi
+      error.should == "can't be blank"
+    end
   end
+
 end
