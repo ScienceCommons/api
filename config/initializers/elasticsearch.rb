0a5db7f5e2c05f057bfda97102c2d6c087bd5c14
bcoe <bencoe@gmail.com>
2014-04-26 22:50:27 -0700

    fixed index name.

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,2 +1,2 @@
 ElasticMapper.server = Stretcher::Server.new(ENV['ELASTIC_SEARCH_URL'] || 'http://127.0.0.1:9200')
-ElasticMapper.index_name = "papersearch_#{ENV['RAILS_ENV']}"
+ElasticMapper.index_name = "curatescience_#{ENV['RAILS_ENV']}"
