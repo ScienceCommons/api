b1f68748567a7664b13f0057db37fa57e8d34c47
bcoe <bencoe@gmail.com>
2014-04-27 00:52:11 -0700

    updated environment variables.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,3 @@
-ENV["REDISTOGO_URL"] ||= 'redis://127.0.0.1'
-uri = URI.parse(ENV["REDISTOGO_URL"])
+ENV["REDIS_URL"] ||= 'redis://127.0.0.1'
+uri = URI.parse(ENV["REDIS_URL"])
 REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,3 +1,3 @@
 ENV["REDISTOGO_URL"] ||= 'redis://127.0.0.1'
-uri = URI.parse(ENV["REDISTOGO_URL"])
+uri = URI.parse(ENV["REDIS_URL"])
 Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :thread_safe => true)
