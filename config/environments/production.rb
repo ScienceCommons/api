6dc194a356b3058f933ed83609e90b5dc787fbab
Stephen Demjanenko <sdemjanenko@gmail.com>
2014-04-29 20:46:59 -0700

    Config: update asset precompile config to include beta.js

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ PaperSearchApi::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( bootstrap.css )
+  config.assets.precompile += %w( bootstrap.css beta.js )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
