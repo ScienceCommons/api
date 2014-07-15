9bd30dd7db485c8842d4c9156815e3d03fb1b9a9
Christian Battista <battista.christian@gmail.com>
2014-07-14 23:24:02 -0700

    models and unit tests

---
+++ app/models/pubmed_indexer.rb
@@ -0,0 +1,3 @@
+#pubmed_indexer.rb
+
+#http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&retmode=xml&WebEnv=NCID_1_842998244_130.14.18.34_9001_1405403552_1417221612&query_key=1&retstart=15&retmax=5&email=battista.christian@gmail.com&tool=curatescience
---
+++ spec/models/pubmed_enqueuer_spec.rb
@@ -0,0 +1,3 @@
+#pubmed_enqueuer_spec.rb
+
+url  = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=psychological%20science[journal]+2014[pdat]&usehistory=y&retstart=0&retmax=5"
