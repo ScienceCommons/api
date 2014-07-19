3cfba3f0732558b44c4c567b18309193036af7c9
Stephen Demjanenko <sdemjanenko@gmail.com>
2014-07-19 13:54:19 -0700

    Beta: set current user and redirect if there is no user

--- app/controllers/beta_controller.rb
+++ app/controllers/beta_controller.rb
@@ -1,5 +1,9 @@
 class BetaController < ApplicationController
+  before_action :current_user
+
   def index
+    return redirect_to("/login") unless @current_user
+
     @main_js = "//s3.amazonaws.com/www.curatescience.org/assets/main.js"
     if ENV["MAIN_JS_VERSION"]
       @main_js = "//s3.amazonaws.com/www.curatescience.org/assets/main-#{ENV["MAIN_JS_VERSION"]}.js"
