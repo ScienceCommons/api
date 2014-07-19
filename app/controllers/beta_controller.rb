ba20367140bb4c641b4eda0a5e597293510b780a
Stephen Demjanenko <sdemjanenko@gmail.com>
2014-07-19 13:10:56 -0700

    Beta#index: serve the page from s3

--- app/controllers/beta_controller.rb
+++ app/controllers/beta_controller.rb
@@ -1,2 +1,8 @@
 class BetaController < ApplicationController
+  def index
+    @main_js = "http://www.curatescience.org.s3-website-us-east-1.amazonaws.com/assets/main.js"
+    if ENV["MAIN_JS_VERSION"]
+      @main_js = "http://www.curatescience.org.s3-website-us-east-1.amazonaws.com/assets/main-#{ENV["MAIN_JS_VERSION"]}.js"
+    end
+  end
 end
