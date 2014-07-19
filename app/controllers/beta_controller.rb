026ad006ebfabd344a3f0b68fb80b47ed18d3fa5
Stephen Demjanenko <sdemjanenko@gmail.com>
2014-07-19 13:19:57 -0700

    Fix main.js link so it supports https

--- app/controllers/beta_controller.rb
+++ app/controllers/beta_controller.rb
@@ -1,8 +1,8 @@
 class BetaController < ApplicationController
   def index
-    @main_js = "http://www.curatescience.org.s3-website-us-east-1.amazonaws.com/assets/main.js"
+    @main_js = "//s3.amazonaws.com/www.curatescience.org/assets/main.js"
     if ENV["MAIN_JS_VERSION"]
-      @main_js = "http://www.curatescience.org.s3-website-us-east-1.amazonaws.com/assets/main-#{ENV["MAIN_JS_VERSION"]}.js"
+      @main_js = "//s3.amazonaws.com/www.curatescience.org/assets/main-#{ENV["MAIN_JS_VERSION"]}.js"
     end
   end
 end
