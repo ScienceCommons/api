e7a02b34106d0153204686b5713d0ae8ec45aa83
Stephen Demjanenko <sdemjanenko@gmail.com>
2014-07-20 11:37:01 -0700

    I think the domain :all was resulting in a different cookie per subdomain
    This was resulting in a csrf issue

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-PaperSearchApi::Application.config.session_store :cookie_store, key: '_science_commons_api_session', :domain => :all, secure: Rails.env == 'production'
+if Rails.env == 'production'
+  PaperSearchApi::Application.config.session_store :cookie_store, key: '_science_commons_api_session', :domain => "curatescience.org", secure: true
+else
+  PaperSearchApi::Application.config.session_store :cookie_store, key: '_science_commons_api_session'
+end
