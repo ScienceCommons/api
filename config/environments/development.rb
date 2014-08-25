14c57283dacfe70180afd01f01a009f388e473e8
bcoe <bencoe@gmail.com>
2014-08-24 21:29:16 -0700

    fixed default url.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,5 +28,5 @@ PaperSearchApi::Application.configure do
   config.assets.debug = true
 
   # environment-specific globals.
-  INVITE_URL = 'https://127.0.0.1:5000/login'
+  INVITE_URL = 'http://127.0.0.1:5000/login'
 end
