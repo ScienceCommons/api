44f7159f2a266ec66ec32dc2032139ef781e404b
bcoe <bencoe@gmail.com>
2014-08-24 21:32:14 -0700

    updated schema.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140805055451) do
+ActiveRecord::Schema.define(version: 20140824173025) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -84,6 +84,7 @@ ActiveRecord::Schema.define(version: 20140805055451) do
     t.integer  "comment_count",    default: 0
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "anonymous"
   end
 
   add_index "comments", ["commentable_id"], name: "index_comments_on_commentable_id", using: :btree
