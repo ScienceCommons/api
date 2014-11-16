d55f81dfd6e8fe8c59910d0790fe3f3f0653453c
Stephen Demjanenko <sdemjanenko@gmail.com>
2014-11-16 10:48:38 -0800

    Sentry: log when an error is caught in the test environment
    Errors were getting caught in controllers making it hard to know if a
    test was failing because of a raised exception.

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -3,6 +3,7 @@ require 'raven'
 Raven.configure do |config|
   config.environments = %w[ production ]
   config.dsn = ENV['SENTRY_DSN'] || '127.0.0.1'
+  config.logger = Logger.new(STDOUT) if Rails.env == "test"
   config.async = lambda { |event|
     Thread.new { Raven.send(event) }
   }
