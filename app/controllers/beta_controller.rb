fc164bd08977fd2d7acaf983219d8707a3118502
Stephen Demjanenko <sdemjanenko@gmail.com>
2014-11-17 20:29:54 -0800

    BetaController#index: allow passage of test_js param to load test JS

--- app/controllers/beta_controller.rb
+++ app/controllers/beta_controller.rb
@@ -2,9 +2,10 @@ class BetaController < ApplicationController
   before_action :current_user
 
   def index
-    @main_js = "//s3.amazonaws.com/www.curatescience.org/assets/main.js"
+    prefix = params[:test_js] ? "test" : "www"
+    @main_js = "//s3.amazonaws.com/#{prefix}.curatescience.org/assets/main.js"
     if ENV["MAIN_JS_VERSION"]
-      @main_js = "//s3.amazonaws.com/www.curatescience.org/assets/main-#{ENV["MAIN_JS_VERSION"]}.js"
+      @main_js = "//s3.amazonaws.com/#{prefix}.curatescience.org/assets/main-#{ENV["MAIN_JS_VERSION"]}.js"
     end
   end
 end
