7cf80cfdb207483648499164c985fe62dfa7d4d8
Alex Kyllo <alex.kyllo@gmail.com>
2015-04-28 22:32:08 -0700

    comment out secure:true in session_store.rb to see if oauth2 works

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 if Rails.env == 'production'
-  PaperSearchApi::Application.config.session_store :cookie_store, key: '_science_commons_api_session', :domain => ENV['DOMAIN'], secure: true
+  PaperSearchApi::Application.config.session_store :cookie_store, key: '_science_commons_api_session', :domain => ENV['DOMAIN']#, secure: true
 else
   PaperSearchApi::Application.config.session_store :cookie_store, key: '_science_commons_api_session'
 end
