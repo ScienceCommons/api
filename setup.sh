6716433d393ea51bac5c9a5120efc9818c5cc4c3
Alex Kyllo <alex.kyllo@gmail.com>
2015-05-25 10:18:11 -0700

    add my headshot to team page

--- setup.sh
+++ setup.sh
@@ -4,3 +4,7 @@ curl -sSL https://get.rvm.io | bash -s $1
 source /usr/local/rvm/scripts/rvm
 rvm install ruby-2.0.0
 rvm use ruby-2.0.0
+#install Java Runtime environment, required for elasticsearch
+apt-get install openjdk-6-jre
+wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.7.deb
+dpkg -i elasticsearch-0.90.7.deb
