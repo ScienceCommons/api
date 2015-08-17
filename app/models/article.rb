f254236691fbc398f2698712abe090d42be22c71
Alex Kyllo <alex.kyllo@gmail.com>
2015-08-16 17:43:06 -0700

    add last updated name date and author id to API feed

--- app/models/article.rb
+++ app/models/article.rb
@@ -57,7 +57,11 @@ class Article < ActiveRecord::Base
   def as_json(opts={})
     super(opts).tap do |h|
       h['authors'] = self.authors if opts[:authors]
-
+      last_update = self.last_model_update
+      submitter = if last_update then last_update.submitter else self.owner end
+      update_author = if submitter then submitter.author else nil end
+      h['updated_by_name'] = if submitter then submitter.name else nil end
+      h['updated_by_author_id'] = if update_author then update_author.id else nil end
     end
   end
 
@@ -113,11 +117,7 @@ class Article < ActiveRecord::Base
     last_model_update.updated_at if last_model_update
   end
 
-  private
     def last_model_update
-      search_objects = []
-      search_objects << self
-      search_objects += self.studies
-      ModelUpdate.where(changeable: search_objects).last
+      ModelUpdate.where(changeable_id: self.id, changeable_type: "Article").last
     end
 end
