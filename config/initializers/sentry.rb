1f7f907f27dc52240c3e1a34708326fcde594830
Alex Kyllo <alex.kyllo@gmail.com>
2015-10-03 13:43:44 -0700

    try to reproduce ssl error on heroku staging

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,10 +1,13 @@
 require 'raven'
 
 Raven.configure do |config|
-  config.environments = %w[ production ]
+  config.environments = %w[ production staging ]
   config.dsn = ENV['SENTRY_DSN'] || '127.0.0.1'
   config.logger = Logger.new(STDOUT) if Rails.env == "test"
   config.async = lambda { |event|
     Thread.new { Raven.send(event) }
   }
+#  config.ssl = {
+#    :ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'
+#  }
 end
