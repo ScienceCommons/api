57d2b7dbce3052931605030cbc09d78e3dd73318
Alex Kyllo <alex.kyllo@gmail.com>
2015-10-03 14:25:34 -0700

    Revert "try to reproduce ssl error on heroku staging"
    This reverts commit 1f7f907f27dc52240c3e1a34708326fcde594830.

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,13 +1,10 @@
 require 'raven'
 
 Raven.configure do |config|
-  config.environments = %w[ production staging ]
+  config.environments = %w[ production ]
   config.dsn = ENV['SENTRY_DSN'] || '127.0.0.1'
   config.logger = Logger.new(STDOUT) if Rails.env == "test"
   config.async = lambda { |event|
     Thread.new { Raven.send(event) }
   }
-#  config.ssl = {
-#    :ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'
-#  }
 end
